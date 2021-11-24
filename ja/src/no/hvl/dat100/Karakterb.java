1d1061d1b55198a28e5791441e91fc4164788608
591312 <89389134+591312@users.noreply.github.com>
2021-11-24 17:41:10 +0100

    Update Karakterb.java

--- ja/src/no/hvl/dat100/Karakterb.java
+++ ja/src/no/hvl/dat100/Karakterb.java
@@ -4,10 +4,9 @@ import static java.lang.Integer.*;
 public class Karakterb {
 
 	public static void main(String[] args) {
-		String karTxt = showInputDialog("Tallkarakter1");
+		String karTxt = showInputDialog("Tallkarakter");
 		int tallKarakter = parseInt (karTxt);
 		String karakter;
-		final int studenter = 10;
 		switch (tallKarakter) {
 		default: karakter = "Ugyldig karakter";
 		if (tallKarakter<101 && tallKarakter>89)
@@ -25,14 +24,11 @@ public class Karakterb {
 		if (tallKarakter<50 && tallKarakter>39)
 			karakter = "E";
 		
-		if (tallKarakter<30 && tallKarakter>-1)
+		if (tallKarakter<40 && tallKarakter>-1)
 			karakter = "F";
 		
-		for (int i = 2; i <= studenter; i++ ) {
-			int personer = Integer.parseInt(showInputDialog("Tallkarakter:" + i ));
 			showMessageDialog(null,karakter);
 	
 		}
 		}
 	}
-}
